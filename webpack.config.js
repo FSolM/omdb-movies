be77e64e0187a01ee27680d884251aa8314bdf51
FSolM <fersolmarin@gmail.com>
2020-11-26 17:59:23 -0600

    Webpack Config Added

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  watch: true
+};
