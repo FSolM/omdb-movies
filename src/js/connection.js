abfe53b833b873c64d518f69d8b97500427a88dd
FSolM <fersolmarin@gmail.com>
2020-11-26 18:45:16 -0600

    Movie Page Iterator Implemented

--- src/js/connection.js
+++ src/js/connection.js
@@ -28,10 +28,28 @@ const APICall = (query, url, key, movies) => {
         movies.push(entry);
       });
 
+      return content.totalResults;
+    })
+    .then((results) => {
+      for (let i = 2; i <= results; i++) {
+        fetch(`${API_url}&page=${i}`)
+          .then((response) => response.json())
+          .then((content) => {
+            content.Search.forEach((entry) => {
+              movies.push(entry);
+            });
+          })
+          .catch((error) => {
+            console.error(`There was a problem while iterating through all the movies: iterator ${i}`);
+            console.error(error);
+          });
+      }
+    })
+    .then(() => {
       dataHandler(movies);
     })
     .catch((error) => {
       console.error('There was an error while communicating with the API');
       console.error(error);
-    })
+    });
 }
